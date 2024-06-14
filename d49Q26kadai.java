3cc17aeccb6a61415ca03adeb6909d45056a486f
s23016 <s23016@std.it-college.ac.jp>
2024-06-14 03:57:05 +0000

    d49Q26kadai

---
+++ d49Q26kadai.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class d49Q26kadai {
+		public static void main(String[] args) {
+				d49Q26kadai obj = new d49Q26kadai();
+				obj.show(args[0]);
+		}
+
+		void show(String n) {
+				System.out.println("value is " + n);
+		}
+}
