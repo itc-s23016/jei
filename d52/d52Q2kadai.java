958b69c10a48e89ed5294b0eec03f8649a61e9c8
s23016 <s23016@std.it-college.ac.jp>
2024-08-02 01:14:53 +0000

    第５２回問２の課題

---
+++ d52/d52Q2kadai.java
@@ -0,0 +1,18 @@
+//s23016
+//d52Q2の問題をアレンジして、実行ができるように修正した。
+
+class Temper {
+				private double temperature;
+				public double getTemperature() { return temperature; }
+				public void setTemperature(double temperature) {
+								this.temperature = temperature;
+				}
+}
+public class d52Q2kadai {
+				public static void main(String[] args) {
+								Temper t = new Temper();
+								t.setTemperature(17.2);
+								System.out.println(t.getTemperature());
+				}
+}
+								
